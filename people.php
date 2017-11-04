5ccb329b83309adf279db782b02f56333812e4b1
Matthew Lausch <mclausch@gmail.com>
2017-11-03 22:05:32 -0700

    new images and people page prep

--- people.php
+++ people.php
@@ -11,19 +11,36 @@ include("head.html")
 	<div class="row">
 		<div class="col-md-8">
 			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Kate Marsh</strong></h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Nullam convallis, arcu vel pellentesque sodales, nisi est varius diam, ac ultrices sem ante quis sem. Proin ultricies volutpat sapien, nec scelerisque ligula mollis lobortis.</p>
-			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Mike Jutan</strong></h3>
-			<p>Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc vehicula lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing <span class="alternative-font">metus</span> sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula. Fusce eget metus lorem, ac viverra leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula.</p>
-			<!--
-			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-			-->
 		</div>
 		<div class="col-md-4">
-<?php insertPhotos("img/mike"); ?>
+<?php insertPhotos("img/kate", array("lkasjf laskdjf asf", "lksjdf")); ?>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-8">
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Mike Jutan</strong></h3>
+		</div>
+		<div class="col-md-4">
+<?php insertPhotos("img/mike", array("lkasjf laskdjf asf", "lksjdf")); ?>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-8">
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Sarah's Family</strong></h3>
+		</div>
+		<div class="col-md-4">
+<?php insertPhotos("img/sarahfamily"); ?>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-8">
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Matthew's Family</strong></h3>
+		</div>
+		<div class="col-md-4">
+<?php insertPhotos("img/matthewfamily"); ?>
 		</div>
 	</div>
 </div>

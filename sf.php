c01310a818fe20e7d5ef13b989fa666d4ddba287
Matthew Lausch <mclausch@gmail.com>
2017-11-30 12:13:00 -0800

    updates

--- sf.php
+++ sf.php
@@ -12,6 +12,17 @@ include("head.html")
 		<div class="col-md-12">
 			<p>Check back soon for tons of details about all of our favourite things in San Francisco!</p>
 		</div>
+<!--
+- high level fluff
+- norcal overview
+- bay area overview
+- sf neighbourhood overview
+- climate/micro-climate
+- links to sub-sections (covering restos, shopping, transit)
+	- around the Kimpton (link back on hotel page)
+	- around the Holiday Inn (link back on hotel page)
+	- around the Presidio
+-->
 	</div>
 </div>
 

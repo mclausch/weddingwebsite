cfecad764f34d086ab2a3899aabbe2d26b17628d
Matthew Lausch <mclausch@gmail.com>
2018-01-15 11:00:17 -0800

    SF starting stuff

--- sf.php
+++ sf.php
@@ -9,20 +9,47 @@ include("head.html")
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-12">
-			<p>Check back soon for tons of details about all of our favourite things in San Francisco!</p>
+		<div class="col-md-8">
+			<p>The city of San Francisco is a small square, 7 miles on a side, located at the northern tip of the San Francisco Peninsula, which divides the San Francisco Bay from the Pacific Ocean. The greater San Francisco region is known as the Bay Area and includes cities like Oakland, Berkeley, San Jose, San Rafael, Santa Rosa, Palo Alto, Mountain View, and many more. The Bay Area is home to Silicon Valley, the Napa and Sonoma wine regions, beautiful beaches, amazing national and state parks, countless museums and event spaces, hundreds of world-class restaurants, spectacular bridges, and an amazing variety of plant and animal life.</p>
+			<p>The Bay Area is divided up into 5 major regions: the City of San Francisco (or just "The City"), East Bay, North Bay, the Peninsula, and South Bay. The City of San Francisco is divided up into a huge number of tiny, walkable neighbourhoods all with distinct and interesting features and characteristics. The hilly nature of the city produces what are known as "micro-climates" &mdash; the many different neighbourhoods all also experience greatly varying weather. Bring layers!</p>
+			<p>The Bay Area is the gateway to Northern California and the Pacific Northwest, a vast area of enormous redwoods, temperate climates, and gorgeous national parks.</p>
 		</div>
-<!--
-- high level fluff
-- norcal overview
-- bay area overview
-- sf neighbourhood overview
-- climate/micro-climate
-- links to sub-sections (covering restos, shopping, transit)
-	- around the Kimpton (link back on hotel page)
-	- around the Holiday Inn (link back on hotel page)
-	- around the Presidio
--->
+		<div class="col-md-4">
+			<img class="img-responsive img-rounded mb-lg" src="img/bayarea.png" alt="Bay Area regions map">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<hr/>
+			<p>In the following pages we've gathered together lists and information about our favourite restaurants, bars, and activities in the Bay Area. Please take a look and as always feel free to <a class="body-link" href="mailto:matthewandsarah2017@gmail.com">reach out</a> to us if you have any questions.</a>
+			<table class="table">
+				<tbody>
+					<tr>
+						<td nowrap="nowrap"><strong><a class="body-link" href="eat.php">Restaurants &amp; Food</a></strong></td>
+						<td>Our favourite thing to do in the city, eat! A listing of our favourite restaurants and food markets.</td>
+					</tr>
+					<tr>
+						<td nowrap="nowrap"><strong><a class="body-link" href="drink.php">Drinking</a></strong></td>
+						<td>Caf&#233;s, Bars, Breweries, Wineries, Distilleries...San Francisco has it all.</td>
+					</tr>
+					<tr>
+						<td nowrap="nowrap"><strong><a class="body-link" href="drink.php">Activities</a></strong></td>
+						<td>Hiking, biking, museums, shopping, sightseeing, tours, parks, bison, beaches, and more.</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<hr/>
+
+			<p>In addition to the pages of lists above, we've created a <a href="https://drive.google.com/open?id=1Wsad_ctX5PhXNhI0BLrNKqU8f5Q&usp=sharing" target="_blank">Google Map</a> with markers for all our favourite spots. If you "star" the map while logged into your Google account then you'll be able to view it from the Google Maps app on your phone (click the star icon beside the title below).</p>
+			<hr/>
+
+			<div class="embed-responsive embed-responsive-4by3">
+				<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1Wsad_ctX5PhXNhI0BLrNKqU8f5Q" frameborder="0"></iframe>
+			</div>
+
+			<hr/>
+		</div>
 	</div>
 </div>
 

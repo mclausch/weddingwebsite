1419a19afc1b4bf394da581cfd1633d74ffbd24f
Matthew Lausch <mclausch@gmail.com>
2018-01-21 22:36:34 -0800

    updates

--- do.php
+++ do.php
@@ -23,8 +23,9 @@ include("head.html")
 	</div>
 	<div class="row">
 		<div class="col-md-12">
+			<a name="handw"></a>
 			<hr/>
-			<h3 class="heading-<?php echo $gPageColor; ?>"><a name="handw"></a>Hikes and Walks</h3>
+			<h3 class="heading-<?php echo $gPageColor; ?>">Hikes and Walks</h3>
 		</div>
 	</div>
 	<div class="row">
@@ -42,6 +43,8 @@ include("head.html")
 	<div class="row">
 		<div class="col-md-12">
 			<p>But you don't have to leave the city to do some quality hiking! Despite its small size, San Francisco packs a ton of awesome walks and hikes covering a variety of terrain and sights. We've compiled a map containing nine great walks in the city of varying lengths and difficulties (see below). All of the hikes are visible in the map by default, but you can use the map menu (top left icon) to hide ones you're not interested in. Most of the walks begin and end at either a restaurant/bar/caf&#233; or other place of note and all of them contain some truly spectacular views.</p>
+			<p>Check out the "Hilly Loop" walk on a clear day to see the city from some of its tallest peaks. Explore the Presidio (where the wedding will take place) with the "Presidio Overlooks," "Crissy Field Loop," and "Golden Gate Bridge Loop" hikes. See where our engagement photos were taken by doing the "Lands End Loop" or just enjoy the Pacific Ocean with the "Ocean Beach Walk." You can also walk to the ocean through San Francisco's enormous Golden Gate Park with the "Golden Gate Park Traverse." Finally, visit some of the more traditional tourist areas in the city with the "Waterfront Stroll" or the "Telegraph Hill" walks. All of the walks are labelled with distances and elevation gains.</p>
+			<p>One last fun walk is to cross the Golden Gate Bridge on foot. Head to the Golden Gate Bridge Visitor Center in the Presidio and walk all or part of its 1.7 mile length on the city side of the road. At the very least be sure to make it to the first pylon and look straight up.</p>
 			<div class="embed-responsive embed-responsive-16by9">
 				<iframe src="https://www.google.com/maps/d/u/0/embed?mid=1aDSe8zZEuxuCIHS3RqRkuZiNqDbMGttW" frameborder="0"></iframe>
 			</div>
@@ -50,36 +53,59 @@ include("head.html")
 
 	<div class="row">
 		<div class="col-md-12">
+			<a name="phys"></a>
 			<hr/>
-			<h3 class="heading-<?php echo $gPageColor; ?>"><a name="phys"></a>Other Physical Activities</h3>
+			<h3 class="heading-<?php echo $gPageColor; ?>">Other Physical Activities</h3>
+			<p>Although we prefer walking, biking is also a fantastic way to get around and see the city. There are a number of great bike rental spots, but our favourite is <a class="body-link" href="http://bikegoldengatebridge.com/" target="_blank">Basically Free Bike Rentals</a> which is run by the discount/warehouse outdoors store <a class="body-link" href="https://shop.sportsbasement.com/" target="_blank">Sports Basement</a>. You can rent a bike from them near Fisherman's Wharf and then you get the value of your rental in store credit to an amazing sporting goods store (credit is good for three days only). The rental location is super convenient for biking through the Presidio and across the Golden Gate Bridge. If you're feeling ambitious, you can even bike on to Sausalito or Tiburon in North Bay, take a ferry back to downtown San Francisco, and then bike along the waterfront to return the bike.</p>
+			<p>If you're looking for a workout, you can always try Matthew's climbing gym, <a class="body-link" href="https://planetgranite.com/sf/" target="_blank">Planet Granite</a> located near the foot of the Golden Gate Bridge in the Presidio. You can climb while enjoying spectacular views of the bridge, the bay, and the city.</p>
+			<p>The city of Sausalito, just across the Golden Gate Bridge, has a company that offers <a class="body-link" href="https://www.seatrek.com/rentals/" target="_blank">kayak and SUP rentals</a> if you'd like to get a bit closer to the water. It's also a great place to visit for an afternoon for sushi and art shops.</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12">
+			<a name="muse"></a>
 			<hr/>
-			<h3 class="heading-<?php echo $gPageColor; ?>"><a name="muse"></a>Museums</h3>
+			<h3 class="heading-<?php echo $gPageColor; ?>">Museums</h3>
+			<p>San Francisco has a ton of fun museums, with something for everyone. Sarah and Matthew enjoyed their second date at the <a class="body-link" href="https://www.calacademy.org/" target="_blank">California Academy of Sciences</a> located in Golden Gate Park. It's a natural history museum with aquariums, a planetarium, and a 4-story, climate-controlled rainforest environment filled with butterflies.</p>
+			<p>Another great science museum is the <a class="body-link" href="https://www.exploratorium.edu/" target="_blank">Exploratorium</a> located on the waterfront. It's a huge interactive science and engineering museum that is super fun for people of all ages.</p>
+			<p>If you're interested in art, San Francisco has the newly remodelled <a class="body-link" href="https://www.sfmoma.org/" target="_blank">SF MoMA</a> (San Francisco Museum of Modern Art), a fantastic place. There are also the more traditional <a class="body-link" href="https://deyoung.famsf.org/" target="_blank">de Young Fine Arts Museum</a> and the <a class="body-link" href="https://legionofhonor.famsf.org" target="_blank">Legion of Honor</a>, sister museums located in unique, spectacular buildings and featuring ancient and contemporary art from America and the rest of the world.</p>
+			<p>One final, off the beaten path museum that you may want to check out is the <a class="body-link" href="https://waltdisney.org/" target="_blank">Walt Disney Family Museum</a>. A completely separate institution from the Disney company, the museum is run by Walt Disney's family and features information about his life and the early days of the animation company he created. It features tons of early drawings, cartoons, films, and music and is located in the Presidio.</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12">
+			<a name="chill"></a>
 			<hr/>
-			<h3 class="heading-<?php echo $gPageColor; ?>"><a name="chill"></a>Chillin'</h3>
+			<h3 class="heading-<?php echo $gPageColor; ?>">Chillin'</h3>
+			<p>If you just want to relax, there are plenty of more chill options in the city too. Locals love to sit around in one of the many parks on sunny days and enjoy drinks and snacks. Check out <a class="body-link" href="http://sfrecpark.org/destination/mission-dolores-park/" target="_blank">Dolores Park</a> in the Mission, or the <a class="body-link" href="https://www.presidio.gov/places/west-bluff-picnic-area" target="_blank">West Bluff Picnic Area</a> in the Presidio. Or grab a spot on the hill in <a class="body-link" href="http://sfrecpark.org/destination/alamo-square/" target="_blank">Alamo Square</a> and spend some time gazing at the "Painted Ladies," Victorian/Edwardian houses that exemplify the style of many traditional homes in San Francisco.</p>
+			<p>If you want to check out a movie, the <a class="body-link" href="https://www.amctheatres.com/movie-theatres/san-francisco/amc-dine-in-kabuki-8" target="_blank">Kabuki Theatre</a> is located close to the wedding hotels and is a great spot where you can get food and drinks while you watch the movie. Or if you're down in the Mission, be sure to check out the <a class="body-link" href="https://drafthouse.com/sf" target="_blank">Alamo Drafthouse</a>, a fantastic independent chain of theatres featuring excellent, full-service in-movie dining and drinks, with custom menus and cocktails tailored to the movie you're seeing.</p>
+			<p>For something different, check out the <a class="body-link" href="https://www.exploratorium.edu/visit/wave-organ" target="_blank">Wave Organ</a>, built by the the people who run the Exploratorium Museum. It's a spot at the very end of a jetty into the bay where pipes allow you to hear the waves and surf from with a new perspective. Be sure to go at high tide for the best experience.</p>
+			<p>If you're into mini-golf, hit up <a class="body-link" href="http://www.urbanputt.com/" target="_blank">Urban Putt</a> in the Mission. It's an indoor miniature golf course that also has great food and adult beverages.</p>
+			<p>Last, but not least, Ocean Beach is a great place to experience the Pacific Ocean. It's both long and deep so even if it's a busy day you can easily find some space away from the crowds. It's often foggy and the water is always cold, but it's a great place for a slow walk or to just sit and watch the waves crashing. Don't plan on swimming though...the rip currents are notorious and regularly claim lives. Also check out <a class="body-link" href="https://www.nps.gov/goga/planyourvisit/cliff-house-sutro-baths.htm" target="_blank">Sutro Baths</a> at the north end of the beach.</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12">
+			<a name="tour"></a>
 			<hr/>
-			<h3 class="heading-<?php echo $gPageColor; ?>"><a name="tour"></a>Must-See Tourist Attractions</h3>
+			<h3 class="heading-<?php echo $gPageColor; ?>">Must-See Tourist Attractions</h3>
+			<p>Like all world-famous cities, San Francisco is full of more traditional "tourist" activities (and tourist traps). Even if you'd traditionally shy away from such popular "tourist" activities, many of them in the Bay Area are quite worthwhile. The Golden Gate Bridge is fantastic and totally worth crossing if you can (by foot, bike, car, or all three). <a class="body-link" href="http://www.sftodo.com/sanfrancisco/cable-car-san-francisco/" target="_blank">Riding a Cable Car</a> is silly but surprisingly fun, and <a class="body-link" href="http://sfrecpark.org/destination/telegraph-hill-pioneer-park/coit-tower/" target="_blank">Coit Tower</a> is worth visiting just for the views and the spectacular steps and homes of Telegraph Hill. <a class="body-link" href="http://www.sftodo.com/sanfrancisco/crooked-street-lombard/" target="_blank">Lombard Street</a> (the "crookedest street in the world") is also fun to walk down.</p>
+			<p>If you're into wildlife, be sure to check out the <a class="body-link" href="http://www.fishermanswharf.org/sea-lion-center.html" target="_blank">sea lions of Fisherman's Wharf</a> but be aware that it is a super tourist-trap type area. Most of the shops are pretty trashy and pick-pockets will be more active in the region. For a more relaxed wildlife experience, visit the <a class="body-link" href="https://goldengatepark.com/buffalo-paddock.html" target="_blank">Golden Gate Park Bison Herd</a>.</p>
+			<p>Another super-touristy activity that is totally worthwhile is to visit <a class="body-link" href="https://www.alcatrazcruises.com/" target="_blank">Alcatraz</a>. It's so much more than just the prison. You can get some of the best views of the city and bay from the island and the ferry ride is a lot of fun. The island is also a sea-bird sanctuary and home to some amazing wildflowers. The prison is also pretty cool with lots of information about the history of the city. It's very popular, so you must book tickets well in advance (like before you arrive in the city).</p>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-12">
+			<a name="shop"></a>
 			<hr/>
-			<h3 class="heading-<?php echo $gPageColor; ?>"><a name="shop"></a>Shopping</h3>
+			<h3 class="heading-<?php echo $gPageColor; ?>">Shopping</h3>
+			<p>San Francisco is part of America, America loves to shop, so San Francisco has lots of great shopping. The main shopping area is around <a class="body-link" href="http://www.visitunionsquaresf.com" target="_blank">Union Square</a> where you'll find all the high-end retailers as well as the <a class="body-link" href="https://www.westfield.com/sanfrancisco" target="_blank">Westfield Mall</a>. The <a class="body-link" href="https://www.ferrybuildingmarketplace.com/" target="_blank">Ferry Building</a> is nearby and an excellent place to find unique food items, crafts, and more.</p>
+			<p>Outside the downtown area, check out Fillmore Street north of Geary; Divisadero Street between McAllister and Haight; Hayes Street between Laguna and Franklin; the region around Irving Street and 9th Avenue; and Clement Street between Arguello and 11th Avenue.</p>
+			<p>One of the great things about San Francisco for shopping is that large national chains are strongly discouraged within the city boundaries so there are unique independent stores all over the place. We particularly enjoy the many small book stores located in every neighbourhood. Our favourite is <a class="body-link" href="http://www.greenapplebooks.com/" target="_blank">Green Apple Books</a>...where we regularly lose hours of our life.</p>
 		</div>
 	</div>
 </div>

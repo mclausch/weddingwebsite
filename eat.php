d7dc4678fe1b03d7717711f18ca5491e2dad31f5
Matthew Lausch <mclausch@gmail.com>
2018-02-05 22:06:13 -0800

    food

--- eat.php
+++ eat.php
@@ -10,9 +10,9 @@ include("head.html")
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<p>For all of you that know us well, you know that we LOVE food. A LOT. So we have of course compiled a list of some of our favorite places to eat in San Francisco. It is an amazing city to live in for Foodies, as SF officially de-throned New York in 2018 as the US city with the most three-Michelin-starred restaurants. SF is a great place to explore varieties of food like Italian, Mexican, Modern American, Japanese, Burmese, Chinese, and so much more. It is a competitive and ever changing landscape of new places to try and amazing food to explore: from a hole in the wall taco place to an extravagant multi-course meal ranging in the hundreds of dollars per person. A big part of the secret to SF's amazing restaurants is the access to all of the bounty of California's enormous agricultural industry. The fresh, local fruits and vegetables are fantastic and it really shows in the final dishes. Explore the many farmer's markets to get the produce straight from the source yourself.</p>
+			<p>For all of you that know us well, you know that we LOVE food. A LOT. So we have of course compiled a list of some of our favorite places to eat in San Francisco. It is an amazing city to live in for Foodies, as SF officially de-throned New York in 2018 as the US city with the most three-Michelin-starred restaurants. SF is a great place to explore varieties of food like Italian, Mexican, Modern American, Japanese, Burmese, Chinese, and so much more. It is a competitive and ever changing landscape of new places to try and amazing food to explore: from a hole in the wall taco place to an extravagant multi-course meal ranging in the hundreds of dollars per person. A big part of the secret to SF's amazing restaurants is the access to all of the bounty of California's enormous agricultural industry. The fresh, local fruits and vegetables are fantastic and it really shows in the final dishes. Explore the many farmers' markets to get the produce straight from the source yourself.</p>
 			<p>Drinks, including beer, wine, spirits, coffee, tea, kombucha, and more are also very excellent in the Bay Area. In fact there's so much to cover there that we've made a separate page for <a class="body-link" href="drink.php">the liquid pleasures</a>.</p>
-			<p>Our big (and incomplete) list below hits food from all over the world and runs the spectrum from ridiculously cheap to insanely expensive. Hopefully there is something for everyone. However, if you'd like to strike out on your own then we recommend two primary websites for research:</p>
+			<p>Our big (and woefully incomplete) list below hits food from all over the world and runs the spectrum from ridiculously cheap to insanely expensive. Hopefully there is something for everyone. However, if you'd like to strike out on your own then we recommend two primary websites for research:</p>
 			<ul class="inline-bullets">
 				<li><a class="body-link" href="https://sf.eater.com/" target="_blank">Eater SF</a> &mdash; Reviews and lists of all the best restaurants in the Bay Area, including articles like these:</li>
 				<ul>
@@ -57,18 +57,142 @@ include("head.html")
 
 			<hr/>
 
-			<!--
-			<p>Below is our bigger list of places, further from the hotels, in and around the Bay Area. We've divided it up into sections based on food type (including desserts!).</p>
 			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Markets and Food Trucks</strong></h3>
+			<p>San Francisco has a ton of food markets to explore and the food truck culture is also very strong. Be sure to check some out for cheap, fresh, and innovative food.</p>
+			<ul>
+				<li><a class="body-link" href="https://www.ferrybuildingmarketplace.com/" target="_blank">Ferry Building</a> &mdash; San Francisco's main ferry terminal is also a massive food hall, and on Saturday mornings is the site of an enormous farmers' market, one of our favourites in the city</li>
+				<li><a class="body-link" href="https://agriculturalinstitute.org/directory/listing-details/clement-st" target="_blank">Clement St Farmers' Market</a> &mdash; Our local farmers' market that runs every Sunday morning and located just three blocks from our apartment</li>
+				<li><a class="body-link" href="http://sfgov.org/realestate/alemany-farmers-market" target="_blank">Alemany Farmers' Market</a> &mdash; The first farmers' market in California, open Saturday</li>
+				<li><a class="body-link" href="https://agriculturalinstitute.org/directory/listing-details/sunday-san-rafael-market" target="_blank">San Rafael Market</a> &mdash; located just north of the city in San Rafael, this epic market is the third largest in California and a hidden gem, open Sunday mornings</li>
+				<li><a class="body-link" href="https://offthegrid.com/" target="_blank">Off the Grid</a> &mdash; A series of food truck festivals located throughout the city, our favourites are the Presidio Picnic on Sundays on the Main Post lawn and the one at Fort Mason on Fridays...but check the website for full details of the 2018 season starting in April</li>
+				<li><a class="body-link" href="http://sparksocialsf.com/" target="_blank">Spark Social SF</a> &mdash; A permanent collection of food trucks located in a newly developed neighbourhood</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>American/Californian</strong></h3>
+			<ul>
+				<li><a class="body-link" href="https://www.zaziesf.com/" target="_blank">Zazie</a> &mdash; great for brunch or dinner, line up for brunch starting at 9am, or make reservations for dinner</li>
+				<li><a class="body-link" href="http://statebirdsf.com" target="_blank">State Bird Provisions</a> &mdash; seasonal American fare served dim sum style. This place is simply amazing. DEFINITELY get reservations 30 days in advance of your trip (or be willing to stand in line for over an hour before the restaurant opens to snag one of the tables reserved for walk-ins)</li>
+				<li><a class="body-link" href="http://frenchsoulfood.com/" target="_blank">Brenda's French Soul Food</a> &mdash; great for breakfast or dinner, be sure to get the beignets!</li>
+				<li><a class="body-link" href="http://brendasmeatandthree.com/" target="_blank">Brenda's Meat & Three</a> &mdash; fantastic sister restaurant to Brend's French Soul Food</li>
+				<li><a class="body-link" href="http://nopasf.com/" target="_blank">Nopa</a> &mdash; classic California cusine, reservations a must</li>
+				<li><a class="body-link" href="http://zunicafe.com/" target="_blank">Zuni Cafe</a> &mdash; a San Francisco institution, get the roast chicken for two</li>
+				<li><a class="body-link" href="http://www.thetipsypigsf.com/" target="_blank">The Tipsy Pig</a> &mdash; great gastropub</li>
+				<li><a class="body-link" href="http://somaeatssf.com/" target="_blank">Soma Eats</a> &mdash; only open on weekdays, but pairs great food with tons of beer</li>
+				<li><a class="body-link" href="http://thegrovesf.com" target="_blank">The Grove</a> &mdash; cheap, easy, tasty, American comfort food, in a casual setting that stays open late</li>
+				<li><a class="body-link" href="https://www.beachchalet.com/" target="_blank">Beach Chalet</a> &mdash; good food with fantastic view of the ocean</li>
+				<li><a class="body-link" href="https://themelt.com/" target="_blank">The Melt</a> &mdash; fancy grilled cheese</li>
+				<li><a class="body-link" href="http://superduperburgers.com/" target="_blank">Super Duper Burger</a> &mdash; a San Francisco-based burger chain that is super-duper!</li>
+				<li><a class="body-link" href="https://www.ateliercrenn.com" target="_blank">Atelier Crenn</a> &mdash; splurge, two Michelin starred restaurant fronted by celebrity chef Dominque Crenn</li>
+				<li><a class="body-link" href="http://www.petitcrenn.com/" target="_blank">Petit Crenn</a> &mdash; also by Dominique Crenn, but at a much more accessible price point</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>BBQ</strong></h3>
+			<ul>
+				<li><a class="body-link" href="www.memphisminnies.com" target="_blank">Memphis Minnie's BBQ Joint</a> &mdash; down to earth BBQ that will amaze your tastebuds, our favorite BBQ in the city</li>
+				<li><a class="body-link" href="http://www.4505burgersandbbq.com/" target="_blank">4505 Burgers & BBQ</a> &mdash; amazing BBQ and a nice outdoor eating area to enjoy it</li>
+				<li><a class="body-link" href="http://www.swinchesterbbq.com/" target="_blank">South Winchester Barbeque</a> &mdash; great beer selection and delicious meats if you find yourself down in San Jose</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Chinese</strong></h3>
+			<ul>
+				<li><a class="body-link" href="https://www.missionchinesefood.com/sfo/" target="_blank">Mission Chinese Food</a> &mdash; delicious Chinese food located in the Mission</li>
+				<li><a class="body-link" href="http://yanksing.com/" target="_blank">Yank Sing</a> &mdash; great Dim Sum downtown</li>
+				<li><a class="body-link" href="http://www.hongkonglounge2.com/" target="_blank">Hong Kong Lounge 2</a> &mdash; great Dim Sum in the Richmond District (i.e. the real chinatown)</li>
+				<li><a class="body-link" href="http://spicessf.com/" target="_blank">Spices</a> &mdash; tasty, spicy noms</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>German</strong></h3>
+			<ul>
+				<li><a class="body-link" href="http://biergartensf.com/" target="_blank">Biergarten</a> &mdash; Bavarian style outdoor beer garden with tasty pretzels and sausages and beer</li>
+				<li><a class="body-link" href="http://www.suppenkuche.com" target="_blank">Suppenkuche</a> &mdash; best German food in the city</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Indian</strong></h3>
+			<ul>
+				<li><a class="body-link" href="http://dosasf.com" target="_blank">Dosa</a> &mdash; delicious South Indian food located in the Fillmore District</li>
+				<li><a class="body-link" href="http://www.curryupnow.com/" target="_blank">Curry Up Now</a> &mdash; tasty Indian food in the Mission</li>
+				<li><a class="body-link" href="http://www.udupipalaceca.com/" target="_blank">Udupi Palace</a> &mdash; more dosa in the Mission</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Italian & Pizza</strong></h3>
+			<ul>
+				<li><a class="body-link" href="http://spqrsf.com" target="_blank">SPQR</a> &mdash; gourmet Italian food and wine</li>
+				<li><a class="body-link" href="https://sfsociale.com/" target="_blank">Sociale</a> &mdash; fine Italian food near our apartment</li>
+				<li><a class="body-link" href="http://www.delpopolosf.com/" target="_blank">Del Popolo</a> &mdash; amazing wood-fired pizza</li>
+				<li><a class="body-link" href="https://pizzetta211.com/" target="_blank">Pizzetta 211</a> &mdash; great little pizza shop, does take-away too</li>
+				<li><a class="body-link" href="http://tonyspizzanapoletana.com/" target="_blank">Tony's Pizza Napoletana</a> &mdash; North Beach (little Italy) classic pizza shop</li>
+				<li><a class="body-link" href="https://www.littlestarpizza.com" target="_blank">Little Star Pizza</a> &mdash; great deep-dish pizza (if that's your thing)</li>
+				<li><a class="body-link" href="https://www.a16pizza.com/" target="_blank">A16</a> &mdash; fantastic pizza and italian food, and spectacular Italian wine</li>
+				<li><a class="body-link" href="http://www.delarosasf.com/" target="_blank">Delarosa</a> &mdash; solid pizza and Italian food</li>
+				<li><a class="body-link" href="https://www.flourandwater.com/" target="_blank">Flour + Water</a> &mdash; super busy Italian in the Mission</li>
+				<li><a class="body-link" href="http://pizzeriadelfina.com/" target="_blank">Pizzeria Delfina</a> &mdash; delicious wood-fired pizza</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Japanese</strong></h3>
+			<ul>
+				<li><a class="body-link" href="http://hinodeyaramen.com" target="_blank">Hinodeya Ramen</a> &mdash; tasty ramen near the Buchanan in Japantown</li>
+				<li><a class="body-link" href="http://mensho.tokyo/" target="_blank">Mensho Tokyo</a> &mdash; Tokyo-based ramen chain, now in SF, expect long lines</li>
+				<li><a class="body-link" href="https://www.tatakisushibar.com" target="_blank">Tataki</a> &mdash; reasonably priced, sustainable sushi relatively near both hotels</li>
+				<li><a class="body-link" href="http://sushiran.com/" target="_blank">Sushi Ran</a> &mdash; classic sushi spot in Sausalito</li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Mexican</strong></h3>
 			<ul>
 				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
 				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
 				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
 				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
 				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Thai</strong></h3>
+			<ul>
 				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
-			</ul>-->
-			<p>Check back soon for further updates to this list, it's a work in progress!</p>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Other</strong></h3>
+			<ul>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+			</ul>
+
+			<hr/>
+
+			<h3 class="heading-<?php echo $gPageColor; ?>"><strong>Desserts and Bakeries</strong></h3>
+			<ul>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+				<li><a class="body-link" href="" target="_blank"></a> &mdash; </li>
+			</ul>
 
 			<hr/>
 		</div>
